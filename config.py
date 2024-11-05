4540d003ca3933480e3689f10ee46a70a7e3b14a
sebastianErixon <sebastian.eriksson@homy.digital>
2024-11-05 04:09:40 +0100

    Env fixed

--- config.py
+++ config.py
@@ -1,16 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # This loads environment variables from .env file
-DATABASE_CONNECTION_STRING = (
-    "Driver={ODBC Driver 17 for SQL Server};"
-    "Server=tcp:yourserver.database.windows.net,1433;"
-    "Database=yourdb;"
-    "Uid=yourusername;"
-    "Pwd=yourpassword;"
-    "Encrypt=yes;"
-    "TrustServerCertificate=no;"
-    "Connection Timeout=30;"
-)
+
 class Config:
-    DATABASE_CONNECTION_STRING = os.getenv("DATABASE_CONNECTION_STRING")
+    DATABASE_CONNECTION_STRING=("Driver={ODBC Driver 17 for SQL Server};"
+                                "Server=tcp:imdbserverdb.database.windows.net,1433;"
+                                "Database=IMDB_DB;"
+                                "Uid=sqluser;"
+                                "Pwd=123qweASD!;Encrypt=yes;"
+                                "TrustServerCertificate=no;"
+                                "Connection Timeout=30;")
