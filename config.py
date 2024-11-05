d36e8e1ff21b706c0082d2f1abae524a256ed4fb
sebastianErixon <sebastian.eriksson@homy.digital>
2024-11-05 03:57:59 +0100

    Changed the structure, more modulaized. Uses blueprints. Seperate routes and db.

--- config.py
+++ config.py
@@ -2,6 +2,15 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()  # This loads environment variables from .env file
-
+DATABASE_CONNECTION_STRING = (
+    "Driver={ODBC Driver 17 for SQL Server};"
+    "Server=tcp:yourserver.database.windows.net,1433;"
+    "Database=yourdb;"
+    "Uid=yourusername;"
+    "Pwd=yourpassword;"
+    "Encrypt=yes;"
+    "TrustServerCertificate=no;"
+    "Connection Timeout=30;"
+)
 class Config:
     DATABASE_CONNECTION_STRING = os.getenv("DATABASE_CONNECTION_STRING")
